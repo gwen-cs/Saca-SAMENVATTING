5ad35236654fec74c8e43c02216f18ceeb49400c
Gwen Reynders <gwenneke@pm.me>
2026-08-28 10:37:43 +0200

    klaar met hoofdstuk 6

--- 6.0_File_options.py
+++ 6.0_File_options.py
@@ -90,3 +90,23 @@ p = p / "ssh" / "sshd_config"
 #   Bestand verwijderen	                os.remove("/tmp/tst/file")	    rm /tmp/tst/file
 #   Lege map verwijderen	            os.rmdir("/tmp/tst/a/b/c")	    rmdir /tmp/tst/a/b/c
 #   Niet-lege map verwijderen	        shutil.rmtree("/tmp/tst/a")	    rm -r /tmp/tst/a
+
+
+# maakt een list van al de directory
+import os
+
+directory_content = os.listdir("./")
+print(directory_content)  # list
+# vb. ['.idea', '.venv', 'demo.txt', 'main.py', 'README.md', 'testmap']
+
+
+# print een lijst af regel per regel met de directory
+import os
+
+for rootdir, dirs, files in os.walk('.'):  # walk is echt bedoeld om te gebruiken in een lus
+    print(f"directory {rootdir} has {len(dirs)} subdirectories and {len(files)} files")
+
+# vb
+# directory .\.idea\inspectionProfiles has 0 subdirectories and 1 files
+# directory .\.venv has 3 subdirectories and 3 files
+# directory .\.venv\Include has 0 subdirectories and 0 files
